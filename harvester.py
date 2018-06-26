03d24fc11abd7672b9f9589be4a1e8ed6d5133fd
jonyfive <22732318+jonyfive@users.noreply.github.com>
2018-06-26 12:27:36 +0300

    Update harvester.py

--- harvester.py
+++ harvester.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #==============================================================================
-# This is a simple generic implementation of a basic web scraper using
+# This is a simple generic implementation of a basic web harvester using
 # BeautifulSoup combined with the Excel(openpyxl) and Word(docx) tools for Python. 
 # It must be properly customized before use. Use at your own risk.
 #==============================================================================
